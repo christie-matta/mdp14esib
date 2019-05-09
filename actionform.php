41a82f8547cfae011c255c801038349ef2aaa605
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-09 15:04:05 +0300

    Update actionform.php

--- actionform.php
+++ actionform.php
@@ -16,34 +16,10 @@
       if ($conn === false)  
       {  
       die(print_r(sqlsrv_errors() , true));  
-      }  
+      } 
+      else { echo("success <br>");}
 
      
-          /*Insert data.*/  
-          $insertSql = "INSERT INTO ContactForm (form_fname,form_lname,form_email,form_message,form_date)   
-  VALUES (?,?,?,?,?)";  
-          $params = array( &$_POST['fname'], &$_POST['lname'], &$_POST['email'] , &$_POST['message'], date("Y-m-d"))
-          );  
-          $stmt = sqlsrv_query($conn, $insertSql, $params);  
-          if ($stmt === false)  
-              {  
-              /*Handle the case of a duplicte e-mail address.*/  
-              $errors = sqlsrv_errors();  
-              if ($errors[0]['code'] == 2601)  
-                  {  
-                  echo " There was a problem. Try Again ! </br>";  
-                  }  
-    
-              /*Die if other errors occurred.*/  
-                else  
-                  {  
-                  die(print_r($errors, true));  
-                  }  
-              }  
-            else  
-              {  
-              echo " Message Sent ! </br>";  
-              }  
       ?>
 
       </body>
