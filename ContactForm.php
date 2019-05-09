f376f857ddcefb56ce1a3275ea2f014828e204d9
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-09 15:24:23 +0300

    Update ContactForm.php

--- ContactForm.php
+++ ContactForm.php
@@ -72,10 +72,10 @@
               </div>
               <br><br><br>
 <form method="post" action="?action=add" enctype="multipart/form-data" >  
- First Name <input type="text" name="fname" id="fname"/></br>  
-Last Name <input type="text" name="lname" id="lname"/></br>  
-email <input type="text" name="email" id="email"/></br>
-message <input type="text" name="message" id="message"/></br>  
+ First Name <input type="text" name="fname" id="fname" class="form-control"/ ></br>  
+Last Name <input type="text" name="lname" id="lname"  class="form-control"/></br>  
+email <input type="text" name="email" id="email"  class="form-control"/></br>
+message <input type="text" name="message" id="message"  class="form-control" /></br>  
 <input type="submit" name="submit" value="Submit" />  
 </form>  
 <?php  
