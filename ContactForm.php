fb8aafe1f72ebe547b071016596b13c7b8728bc0
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-09 14:53:16 +0300

    Update ContactForm.php

--- ContactForm.php
+++ ContactForm.php
@@ -127,7 +127,24 @@
 
       </form> 
      
-      
+       <?php
+
+
+      // database connection and table name
+      $serverName = "tcp:server-mdp.database.windows.net,1433";  
+      $connectionOptions = array(  
+          "Database" => "DB-MDP",  
+          "UID" => "adminmdp",  
+          "PWD" => "p@ssw0rd"  
+      );  
+      $conn = sqlsrv_connect($serverName, $connectionOptions);  
+
+      if ($conn === false)  
+      {  
+      die(print_r(sqlsrv_errors() , true));  
+      }  
+
+      ?>
     
 </body>
 
