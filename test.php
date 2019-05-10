6248e6909eb3eeaa197aaf48b7d15f54b6f021ce
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-10 16:42:25 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
- 
+ include("phpgraphlib.php");
+$graph=new PHPGraphLib(550,350);
+
 $serverName = "tcp:server-mdp.database.windows.net,1433";
 $connectionInfo = array( "Database"=>"DB-MDP", "UID"=>"adminmdp", "PWD"=>"p@ssw0rd");
 $conn = sqlsrv_connect( $serverName, $connectionInfo );
@@ -16,11 +18,17 @@ if( $stmt === false) {
 while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
 	$id=$row["id"];
       $val=$row["val"];
+	$dataArray[$id]=$val;
       echo $row['id'].", ".$row['val']."<br />";
 	
   
 }
 
 sqlsrv_free_stmt( $stmt);
+$graph->addData($dataArray);
+$graph->setTitle("Sales by Group");
+$graph->setGradient("lime", "green");
+$graph->setBarOutlineColor("black");
+$graph->createGraph();
 
 ?>
