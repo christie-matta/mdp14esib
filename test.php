e062e6bfb65b2292e6d2b219aaa9df7cd37c5b32
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-10 18:40:52 +0300

    Update test.php

--- test.php
+++ test.php
@@ -27,4 +27,31 @@ sqlsrv_free_stmt( $stmt);
 
 
 ?>
-
+<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "PHP Column Chart from Database"
+	},
+	data: [{
+		type: "column", //change type to bar, line, area, pie, etc  
+		dataArray: <?php echo json_encode($dataArray, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>    
