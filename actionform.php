9e357921ddf782809dc0081148f0c1ac029f7f28
christie-matta <50423949+christie-matta@users.noreply.github.com>
2019-05-10 11:59:05 +0300

    Add files via upload

--- actionform.php
+++ actionform.php
@@ -1,24 +1,51 @@
+<html>
+    message sent success
+<?php  
+/*Connect using SQL Server authentication.*/  
+$serverName = "tcp:server-mdp.database.windows.net,1433";  
+$connectionOptions = array(  
+    "Database" => "DB-MDP",  
+    "UID" => "adminmdp",  
+    "PWD" => "p@ssw0rd"  
+);  
+$conn = sqlsrv_connect($serverName, $connectionOptions);  
+  
+if ($conn === false)  
+    {  
+    die(print_r(sqlsrv_errors() , true));  
+    }  
+  
 
-   processing
-    <?php
-
-
-      // database connection and table name
-      $serverName = "tcp:server-mdp.database.windows.net,1433";  
-      $connectionOptions = array(  
-          "Database" => "DB-MDP",  
-          "UID" => "adminmdp",  
-          "PWD" => "p@ssw0rd"  
-      );  
-      $conn = sqlsrv_connect($serverName, $connectionOptions);  
-
-      if ($conn === false)  
-      {  
-      die(print_r(sqlsrv_errors() , true));  
-      } 
-      else { echo("success <br>");}
-      
-       
-      ?>
-
-      
+        /*Insert data.*/  
+        $insertSql = "INSERT INTO ContactForm (form_fname,form_lname,form_email,form_message,form_date)   
+VALUES (?,?,?,?,?)";  
+    
+        $params = array( &$_POST['fname'], &$_POST['lname'], &$_POST['email'] ,&$_POST['message']  , date("Y-m-d")
+        );  
+        $stmt = sqlsrv_query($conn, $insertSql, $params);  
+       $row = mysqli_fetch_array($result);
+        if ($stmt === false)  
+            {  
+            /*Handle the case of a duplicte e-mail address.*/  
+            $errors = sqlsrv_errors();  
+            if ($errors[0]['code'] == 2601)  
+                {  
+                 
+                echo " Success </br>";  
+              
+                }  
+  
+            /*Die if other errors occurred.*/  
+              else  
+                {  
+                die(print_r($errors, true));  
+                }  
+            }  
+          else  
+            {  
+            echo "Message sent </br>";  
+           
+  
+    }  
+  ?>
+</html>
