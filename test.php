2c8c3d7abe9463bd1a8d8bf733db0546d25f7a54
christie-matta <50423949+christie-matta@users.noreply.github.com>
2019-05-10 13:45:55 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,44 +1,15 @@
 <?php
-include("phpgraphlib.php");
-$graph=new PHPGraphLib(550,350); 
-/*$link = mysql_connect('localhost', 'username', 'password')
-   or die('Could not connect: ' . mysql_error());
-     
-mysql_select_db('Message') or die('Could not select database');*/
-$serverName = "tcp:server-mdp.database.windows.net,1433";  
 
-$connectionOptions = array(  
-
-    "Database" => "DB-MDP",  
-
-    "UID" => "adminmdp",  
-
-    "PWD" => "p@ssw0rd"  
-
-);  
-
-$conn = sqlsrv_connect($serverName, $connectionOptions); 
-echo("<br> essai <br>");  
-$dataArray=array();
-  
-//get data from database
-$sql="SELECT * FROM Graph";
-$result = mysql_query($sql) or die('Query failed: ' . mysql_error());
-if ($result) {
-  while ($row = mysql_fetch_assoc($result)) {
-      $id=$row["id"];
-      $val=$row["val"];
-      //add to data areray
-      $dataArray[$id]=$val;
-  }
-}
-  
-//configure graph
-$graph->addData($dataArray);
-$graph->setTitle("Value by id");
-$graph->setGradient("lime", "green");
-$graph->setBarOutlineColor("black");
+include('phpgraphlib.php');
+$graph = new PHPGraphLib(500,350);
+$data = array(12124, 5535, 43373, 22223, 90432, 23332, 15544, 24523,
+ 32778, 38878, 28787, 33243, 34832, 32302);
+$graph->addData($data);
+$graph->setTitle('Widgets Produced');
+$graph->setGradient('red', 'maroon');
 $graph->createGraph();
+
+
 ?>
 
 
