d44962ca058fd8328caa1c5c9c84fe48e80aeae5
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-10 16:30:08 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,111 +1,31 @@
 <?php
-
-
-
- 
-$dataPoints = array(
-	array("label"=> 1997, "y"=> 254722.1),
-	array("label"=> 1998, "y"=> 292175.1),
-	array("label"=> 1999, "y"=> 369565),
-	array("label"=> 2000, "y"=> 284918.9),
-	array("label"=> 2001, "y"=> 325574.7),
-	array("label"=> 2002, "y"=> 254689.8),
-	array("label"=> 2003, "y"=> 303909),
-	array("label"=> 2004, "y"=> 335092.9),
-	array("label"=> 2005, "y"=> 408128),
-	array("label"=> 2006, "y"=> 300992.2),
-	array("label"=> 2007, "y"=> 401911.5),
-	array("label"=> 2008, "y"=> 299009.2),
-	array("label"=> 2009, "y"=> 319814.4),
-	array("label"=> 2010, "y"=> 357303.9),
-	array("label"=> 2011, "y"=> 353838.9),
-	array("label"=> 2012, "y"=> 288386.5),
-	array("label"=> 2013, "y"=> 485058.4),
-	array("label"=> 2014, "y"=> 326794.4),
-	array("label"=> 2015, "y"=> 483812.3),
-	array("label"=> 2016, "y"=> 254484)
-);
-	
-?>
-<!DOCTYPE HTML>
-<html>
-<head>  
-<script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	//theme: "light2",
-	title:{
-		text: "Salmon Production - 1997 to 2006"
-	},
-	axisX:{
-		crosshair: {
-			enabled: true,
-			snapToDataPoint: true
-		}
-	},
-	axisY:{
-		title: "in Metric Tons",
-		crosshair: {
-			enabled: true,
-			snapToDataPoint: true
-		}
-	},
-	toolTip:{
-		enabled: false
-	},
-	data: [{
-		type: "area",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
- 
+include("phpgraphlib.php");
+$graph=new PHPGraphLib(550,350); 
+$serverName = "tcp:servername.database.windows.net,1433";
+$connectionInfo = array( "Database"=>"DBName", "UID"=>"Username", "PWD"=>"Password");
+$conn = sqlsrv_connect( $serverName, $connectionInfo );
+if( $conn === false ) {
+    die( print_r( sqlsrv_errors(), true));
 }
-</script>
-</head>
-<body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-	<?php
-	$serverName = "tcp:servername.database.windows.net,1433";  
+$dataArray=array();
+$sql = "SELECT id, val FROM Graph";
+$stmt = sqlsrv_query( $conn, $sql );
+if( $stmt === false) {
+    die( print_r( sqlsrv_errors(), true) );
+}
 
-$connectionOptions = array(  
-    "Database" => "DBName",  
-    "UID" => "Username",  
-    "PWD" => "Password"  
-);  
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+      echo $row['id'].", ".$row['val']."<br />";
+	$id=$row["id"];
+      $val=$row["val"];
+      //add to data areray
+      $dataArray[$id]=$val;
+}
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);  
-  
-if ($conn === false)  
-    {  
-    die(print_r(sqlsrv_errors() , true));  
-    }  
-
-$sql = "SELECT * FROM Graph"; 
-$stmt = sqlsrv_query($conn, $sql); 
-if($stmt === false) 
-{ 
-die(print_r(sqlsrv_errors(), true)); 
-} 
- 
-if(sqlsrv_has_rows($stmt)) 
-{ 
-print("<table border='1px'>"); 
-print("<tr><td>Id</td>"); 
-print("<td>Val</td></tr>"); 
- 
-while($row = sqlsrv_fetch_array($stmt)) 
-{ 
- 
-print("<tr><td>".$row['id']."</td>"); 
-print("<td>".$row['val']."</td></tr>"); 
-} 
- 
-print("</table>"); 
-}*
-	?>
-</body>
-</html>    
+sqlsrv_free_stmt( $stmt);
+$graph->addData($dataArray);
+$graph->setTitle("Sales by Group");
+$graph->setGradient("lime", "green");
+$graph->setBarOutlineColor("black");
+$graph->createGraph();
+?>
