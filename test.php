768b798d2e976d96b148f7b75fc4a1005806c0f9
christie-matta <50423949+christie-matta@users.noreply.github.com>
2019-05-10 13:13:45 +0300

    Update test.php

--- test.php
+++ test.php
@@ -22,20 +22,20 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 $dataArray=array();
   
 //get data from database
-$sql="SELECT RoomNo, COUNT(*) AS 'count' FROM Message GROUP BY RoomNo";
+$sql="SELECT * FROM Graph";
 $result = mysql_query($sql) or die('Query failed: ' . mysql_error());
 if ($result) {
   while ($row = mysql_fetch_assoc($result)) {
-      $salesgroup=$row["RoomNo"];
-      $count=$row["count"];
+      $id=$row["id"];
+      $val=$row["val"];
       //add to data areray
-      $dataArray[$salesgroup]=$count;
+      $dataArray[$id]=$val;
   }
 }
   
 //configure graph
 $graph->addData($dataArray);
-$graph->setTitle("Sales by Group");
+$graph->setTitle("Value by id");
 $graph->setGradient("lime", "green");
 $graph->setBarOutlineColor("black");
 $graph->createGraph();
@@ -43,57 +43,6 @@ $graph->createGraph();
 
 
 
-<?php
- 
-$dataPoints = array();
-//Best practice is to create a separate file for handling connection to database
-try{
-     // Creating a new connection.
-    // Replace your-hostname, your-db, your-username, your-password according to your database
-    $link = new \PDO(   'mysql:host=your-hostname;dbname=your-db;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
-                        'your-username', //'root',
-                        'your-password', //'',
-                        array(
-                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                            \PDO::ATTR_PERSISTENT => false
-                        )
-                    );
-	
-    $handle = $link->prepare('select x, y from datapoints'); 
-    $handle->execute(); 
-    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
-		
-    foreach($result as $row){
-        array_push($dataPoints, array("x"=> $row->x, "y"=> $row->y));
-    }
-	$link = null;
-}
-catch(\PDOException $ex){
-    print($ex->getMessage());
-}
-	
-?>
-<!DOCTYPE HTML>
-<html>
-<head>  
-<script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", // "light1", "light2", "dark1", "dark2"
-	title:{
-		text: "PHP Column Chart from Database"
-	},
-	data: [{
-		type: "column", //change type to bar, line, area, pie, etc  
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
- 
-}
 </script>
 </head>
 <body>
