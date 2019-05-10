5269b3d4c06bd233446d42b6ac07f5dd148cefb2
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-10 17:18:43 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
-
+include("phpgraphlib.php");
+$graph=new PHPGraphLib(550,350); 
 
 $serverName = "tcp:server-mdp.database.windows.net,1433";
 $connectionInfo = array( "Database"=>"DB-MDP", "UID"=>"adminmdp", "PWD"=>"p@ssw0rd");
@@ -19,39 +20,16 @@ while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
       $val=$row["val"];
 	$dataArray[$id]=$val;
       echo $row['id'].", ".$row['val']."<br />";
-	
-  
 }
 
+
 sqlsrv_free_stmt( $stmt);
+$graph->addData($dataArray);
+$graph->setTitle("Sales by Group");
+$graph->setGradient("lime", "green");
+$graph->setBarOutlineColor("black");
+$graph->createGraph();
 
 
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>  
-<script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", // "light1", "light2", "dark1", "dark2"
-	title:{
-		text: "PHP Column Chart from Database"
-	},
-	data: [{
-		type: "column", //change type to bar, line, area, pie, etc  
-		dataPoints: <?php echo json_encode($dataArray, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
- 
-}
-</script>
-</head>
-<body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</body>
-</html>      
+
