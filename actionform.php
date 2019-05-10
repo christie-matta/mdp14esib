8d627db0fe613da627206a40196a09f7085d622a
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-10 16:28:40 +0300

    Update actionform.php

--- actionform.php
+++ actionform.php
@@ -20,8 +20,7 @@
  
 <body style='background-image: linear-gradient(to bottom right,#8AECAF, #06a7cf);'>
        
-
-        <ul class="nav nav-tabs" id="myTab" role="tablist" >
+<ul class="nav nav-tabs" id="myTab" role="tablist" >
                 <li class="nav-item">
                   <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
                 </li>
