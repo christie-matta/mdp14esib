bd965e237fa221897df4e1056a77dc9b0d31a7c8
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-10 19:17:06 +0300

    Update test.php

--- test.php
+++ test.php
@@ -13,7 +13,6 @@ try {
         array_push($dataPoints, array("x"=> $row->id, "y"=> $row->val));
     }
 	$link = null;
-
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
