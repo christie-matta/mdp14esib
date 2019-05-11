d457e67a6d92afcff38f2d70fcd2c861d65f2102
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-11 10:41:46 +0300

    Update test.php

--- test.php
+++ test.php
@@ -32,8 +32,7 @@ catch (PDOException $e) {
 	
 
 	
-	<input type="text" name="datetimes" />
-
+<input type="text" name="datetimes" />
 <script>
 $(function() {
   $('input[name="datetimes"]').daterangepicker({
@@ -43,6 +42,21 @@ $(function() {
     locale: {
       format: 'M/DD hh:mm A'
     }
+  });
+});
+</script>
+	
+	<input type="text" name="birthday" value="10/24/1984" />
+<script>
+$(function() {
+  $('input[name="birthday"]').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    minYear: 1901,
+    maxYear: parseInt(moment().format('YYYY'),10)
+  }, function(start, end, label) {
+    var years = moment().diff(start, 'years');
+    alert("You are " + years + " years old!");
   });
 });
 </script>
