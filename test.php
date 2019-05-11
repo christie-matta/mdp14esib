c227def4f3bd542abfb0b3d02f2358eb0e3495e3
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-11 10:55:29 +0300

    Update test.php

--- test.php
+++ test.php
@@ -29,7 +29,7 @@ catch (PDOException $e) {
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-<br><br>
+
 <input type="text" name="birthday" value="10/24/1984" />
 <script>
 $(function() {
