df6445f6f83b14d84bd5f146d0829068a2e22394
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-11 10:44:32 +0300

    Update test.php

--- test.php
+++ test.php
@@ -49,6 +49,20 @@ $(function() {
 });
 </script>
 	
+input type="text" name="datetimes" />
+<script>
+$(function() {
+  $('input[name="datetimes"]').daterangepicker({
+    timePicker: true,
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    locale: {
+      format: 'M/DD hh:mm A'
+    }
+  });
+});
+</script>
+	
 <script>
 window.onload = function () {
  
