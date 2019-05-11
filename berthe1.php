fa010199e79679be3db5068c27e6ad08be2ce39c
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-11 14:54:32 +0300

    Update berthe1.php

--- berthe1.php
+++ berthe1.php
@@ -94,7 +94,7 @@ if( $stmt === false)
             if(from_date != '' && to_date != '')
             {alert("you ve just entered");
                 $.ajax({
-                    url:"berthe2.php",
+                    url:"https://github.com/christie-matta/mdp14esib/edit/master/berthe2.php",
                     method:"POST",
                     data:{from_date:from_date, to_date:to_date},
                     success:function(data)
