ff28fd0f203685e1c41661228c54458e3f9d6027
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-13 18:38:23 +0300

    Update romy1.php

--- romy1.php
+++ romy1.php
@@ -11,7 +11,8 @@ if( $conn === false )
 }
   if(isset($_POST['filter']))
   {
-	 // echo "tene chi ";
+	 echo ($_POST['from_date']);
+	   echo ($_POST['to_date']);
   $from_date=$_POST['from_date'];
   $to_date=$_POST['to_date'];
   
