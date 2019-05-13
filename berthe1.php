820b265a2a2ea2f77db4b35b97ece225719e94cc
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-13 14:16:38 +0300

    Update berthe1.php

--- berthe1.php
+++ berthe1.php
@@ -97,7 +97,7 @@ if( $stmt === false)
             if(from_date != '' && to_date != '')
             {
                 $.ajax({
-                    url:"https://github.com/christie-matta/mdp14esib/blob/master/berthe2.php",
+                    url:"berthe2.php",
                     method:"POST",
                     data:{from_date:from_date, to_date:to_date},
                     success:function(data)
