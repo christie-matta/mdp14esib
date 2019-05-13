263d8da07e2ac40b27f03bf224f06ad166173c28
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-13 18:31:39 +0300

    Update romy1.php

--- romy1.php
+++ romy1.php
@@ -35,7 +35,7 @@ To: <input type="text" name="to_date">
 </form>
 			<?php
 			if($count>0)
-			{	echo "telit chi ";
+			{	//echo "telit chi ";
 				while($row = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC))
 			{
 					
