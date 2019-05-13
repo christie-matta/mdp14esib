8997e363380aa439e93c3aade441bb4bad1ae86a
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-13 19:22:31 +0300

    Update romy1.php

--- romy1.php
+++ romy1.php
@@ -11,8 +11,8 @@ if( $conn === false )
 }
   if(isset($_POST['filter']))
   {
-	// echo ($_POST['from_date']);
-	 //  echo ($_POST['to_date']);
+	 echo ($_POST['from_date']);
+	   echo ($_POST['to_date']);
 	  if($_POST['from_date']<=$_POST['to_date']){
 	echo(  gettype($_POST['to_date']) );}
   $from_date=$_POST['from_date'];
