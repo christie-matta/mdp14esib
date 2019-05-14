5c94dee546aad18508136134fbe5082c462652eb
christie-matta <50423949+christie-matta@users.noreply.github.com>
2019-05-14 12:58:18 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -1,88 +1,9 @@
 <?php
- 
-$dataPoints = array();
-//Best practice is to create a separate file for handling connection to database
-try {
-    $conn = new \PDO("sqlsrv:server = tcp:server-mdp.database.windows.net,1433; Database = DB-MDP", "adminmdp", "p@ssw0rd");
-    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    $handle = $conn->prepare('select id, val from Graph '); 
-    $handle->execute(); 
-    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
-		
-    foreach($result as $row){
-        array_push($dataPoints, array("x"=> $row->id, "y"=> $row->val));
-    }
-	$link = null;
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-	
+include('phpgraphlib.php');
+$graph = new PHPGraphLib(400,300);
+$data = array("Alex"=>99, "Mary"=>98, "Joan"=>70, "Ed"=>90);
+$graph->addData($data);
+$graph->setTitle("Test Scores");
+$graph->setTextColor("blue");
+$graph->createGraph();
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>  
-	
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />-
-
-	<input type="text" name="birthday" value="10/24/1984" />
-<script>
-$(function() {
-  $('input[name="birthday"]').daterangepicker({
-    singleDatePicker: true,
-    showDropdowns: true,
-    minYear: 1901,
-    maxYear: parseInt(moment().format('YYYY'),10)
-  }, function(start, end, label) {
-    var years = moment().diff(start, 'years');
-    alert("You are " + years + " years old!");
-  });
-});
-</script>
-	
-<input type="text" name="datetimes" />
-<script>
-$(function() {
-  $('input[name="datetimes"]').daterangepicker({
-    timePicker: true,
-    startDate: moment().startOf('hour'),
-    endDate: moment().startOf('hour').add(32, 'hour'),
-    locale: {
-      format: 'M/DD hh:mm A'
-    }
-  });
-});
-</script>
-
-	
-<script>
-window.onload = function () {
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", // "light1", "light2", "dark1", "dark2"
-	title:{
-		text: "Graph"
-	},
-	data: [{
-		type: "column", //change type to bar, line, area, pie, etc  
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	 }]  
-   }); 
-chart.render();
- 
-} 
-
-</script>
-</head>
-<body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</body>
-</html>
