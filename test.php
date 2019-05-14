cd992cc34be03973c6841042c83c211956a34cfe
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-14 18:38:16 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,117 +1,68 @@
-  
-<?php  
-$dataPoints = array();
-try{
-$conn = new \PDO("sqlsrv:server = tcp:server-mdp.database.windows.net,1433; Database = DB-MDP", "adminmdp", "p@ssw0rd");
-    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
- if (isset($_GET['action']))  
-    {  
-    if ($_GET['action'] == 'add')  
-        {  
-        /*Insert data.*/  
-        $debut="SELECT *  FROM Graph WHERE dates>='";
-        $datefrom=&$_POST['from'];
-        $dateto=&$_POST['toto'];
-        $and="' and dates<='";
-        $fin="' order by dates;";
-        $a=$debut.$datefrom;
-            $b=$a.$and;
-            $c=$b.$dateto;
-            $sql=$c.$fin;
-    $handle = $conn->prepare($sql); 
-    $handle->execute(); 
-    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
-		
-    foreach($result as $row){
-        array_push($dataPoints, array(
-		"x"=> $row->id, 
-		"y"=> $row->val));
-				      
-    }
-	$link = null;
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}?>
-
-<!DOCTYPE HTML>
-<html>
-<head>  
-    <Title>Azure SQL Database - PHP Website</Title>  
+<html>  
+  <head>  
+  <Title>Azure SQL Database - PHP Website</Title>  
+  </head>  
+  <body>  
+  <form method="post" action="?action=add" enctype="multipart/form-data" >  
+  From : <input type="date" name="from" id="from"/></br>  
+  To : address <input type="date" name="toto" id="toto"/></br>  
+  <input type="submit" name="submit" value="Submit" />  
+  </form>  
+  <?php  
+  /*Connect using SQL Server authentication.*/  
+  $serverName = "tcp:server-mdp.database.windows.net,1433";
+  $connectionOptions = array(  
+      "Database" => "DB-MDP",  
+      "UID" => "adminmdp",  
+      "PWD" => "p@ssw0rd"  
+  );  
+  $conn = sqlsrv_connect($serverName, $connectionOptions);   
     
-<body>  
-<form method="post" action="?action=add" enctype="multipart/form-data" >  
-From : <input type="date" name="from" id="from"/></br>  
-To : address <input type="date" name="toto" id="toto"/></br>  
-<input type="submit" name="submit" value="Submit" />  
-</form>
+  if ($conn === false)  
+      {  
+      die(print_r(sqlsrv_errors() , true));  
+      }  
     
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<div id="chartContainer1" style="height: 370px; width: 40%;"></div>
-	<br><br>
-
-<div id="chartContainer2" style="height: 370px; width: 40%;"></div>
-    
-<script>
-window.onload = function () {
-
-	var chart = new CanvasJS.Chart("chartContainer1", {
-	animationEnabled: true,
-	theme: "light2",
-	title:{
-		text: "temperature"
-	},
-	axisY:{
-		includeZero: false
-	},
-	data: [{        
-		type: "line",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-		
-	}]
-});
-chart.render();
-	       	var chart = new CanvasJS.Chart("chartContainer2", {
-	animationEnabled: true,
-	theme: "light2",
-	title:{
-		text: "pH"
-	},
-	axisY:{
-		includeZero: false
-	},
-	data: [{        
-		type: "line",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-		
-	}]
-});
-chart.render();
-}
-	    	   
-</script>
-</head>
-
-
-	
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-
- 
+  if (isset($_GET['action']))  
+      {  
+      if ($_GET['action'] == 'add')  
+          {  
+          /*Insert data.*/  
+          $debut="SELECT *  FROM Graph WHERE dates>='";
+          $datefrom=&$_POST['from'];
+          $dateto=&$_POST['toto'];
+          $and="' and dates<='";
+          $fin="';";
+          $a=$debut.$datefrom;
+              $b=$a.$and;
+              $c=$b.$dateto;
+              $sql=$c.$fin;
+          echo($sql);
+          echo(gettype($datefrom));
+        //  $sql =  "SELECT *  FROM Graph WHERE id=( SELECT max( id ) FROM Graph )"; 
+   
+  $stmt = sqlsrv_query($conn, $sql); 
+  if($stmt === false) 
+  { 
+  die(print_r(sqlsrv_errors(), true)); 
+  } 
+   
+  if(sqlsrv_has_rows($stmt)) 
+  { 
+  print("<table border='1px'>"); 
+  print("<tr><td> Id</td>"); 
+  print("<td>Val</td>"); 
+  print("<td>date</td></tr>"); 
+   
+  while($row = sqlsrv_fetch_array($stmt)) 
+  { 
+   
+  print("<tr><td>".$row['id']."</td>"); 
+  print("<td>".$row['val']."</td>"); 
+  print("<td>".$row['dates']."</td></tr>"); 
+  } 
+   
+  print("</table>"); 
+  }
+          }  
+      } 
