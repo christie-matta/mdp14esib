a6c7c1b931f14d38ebc61e4c22eeae5e0125d3ea
BertheHajjar <50440843+BertheHajjar@users.noreply.github.com>
2019-05-14 18:44:25 +0300

    Update tejrib.php

--- tejrib.php
+++ tejrib.php
@@ -20,7 +20,7 @@ try {
               $b=$a.$and;
               $c=$b.$dateto;
               $sql=$c.$fin;
-    $handle = $conn->prepare($sql); 
+    $handle = $conn->prepare('$sql'); 
     $handle->execute(); 
     $result = $handle->fetchAll(\PDO::FETCH_OBJ);
 		
