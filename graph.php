2babd97b1bb75a205b1c1fe7b369d754f35e2be4
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-14 18:09:25 +0300

    Update graph.php

--- graph.php
+++ graph.php
@@ -2,29 +2,21 @@
  
 $dataPoints = array();
 //Best practice is to create a separate file for handling connection to database
-try{
-     // Creating a new connection.
-    // Replace your-hostname, your-db, your-username, your-password according to your database
-    $link = new \PDO(   'mysql:host=your-hostname;dbname=your-db;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
-                        'your-username', //'root',
-                        'your-password', //'',
-                        array(
-                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                            \PDO::ATTR_PERSISTENT => false
-                        )
-                    );
-	
-    $handle = $link->prepare('select val from Graph'); 
+try {
+    $conn = new \PDO("sqlsrv:server = tcp:server-mdp.database.windows.net,1433; Database = DB-MDP", "adminmdp", "p@ssw0rd");
+    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    $handle = $conn->prepare('select id, val from Graph '); 
     $handle->execute(); 
     $result = $handle->fetchAll(\PDO::FETCH_OBJ);
 		
     foreach($result as $row){
-        array_push($dataPoints, array("x"=> $row->val, "y"=> $row->y));
+        array_push($dataPoints, array("x"=> $row->id, "y"=> $row->val));
     }
 	$link = null;
 }
-catch(\PDOException $ex){
-    print($ex->getMessage());
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 	
 ?>
@@ -34,25 +26,67 @@ catch(\PDOException $ex){
 <script>
 window.onload = function () {
  
-var chart = new CanvasJS.Chart("chartContainer", {
+/*var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	exportEnabled: true,
 	theme: "light1", // "light1", "light2", "dark1", "dark2"
 	title:{
-		text: "PHP Column Chart from Database"
+		text: "Graph"
 	},
 	data: [{
 		type: "column", //change type to bar, line, area, pie, etc  
 		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	 }]  
+   }); 
+chart.render();
+ 
+} */
+	
+	
+	var chart = new CanvasJS.Chart("chartContainer1", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Temperature"
+	},
+	axisY:{
+		includeZero: false
+	},
+	data: [{        
+		type: "line",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+		
 	}]
 });
 chart.render();
- 
+	       	var chart = new CanvasJS.Chart("chartContainer2", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "pH"
+	},
+	axisY:{
+		includeZero: false
+	},
+	data: [{        
+		type: "line",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+		
+	}]
+});
+chart.render();
 }
+	    	       
+	
 </script>
 </head>
 <body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<div id="chartContainer1" style="height: 370px; width: 40%;"></div>
+	<br><br>
+
+<div id="chartContainer2" style="height: 370px; width: 40%;"></div>
+
+	
 </body>
-</html>   
+</html>
