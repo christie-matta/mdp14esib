8b212d9b53a4026e7558474b864c4e94ab0bb1c0
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-15 20:57:14 +0300

    Update now.php

--- now.php
+++ now.php
@@ -1,6 +1,6 @@
 <html>  
 <head>  
-<Title>Live updates-Lebanese Cost Line Water and Air Quality Monitoring</Title> 
+<Title>Latest Updates-Lebanese Cost Line Water and Air Quality Monitoring</Title> 
 <META name="author" content="Romy Charbel" >
 <META charset="UTF-8">
 <META name="description" content="Page measurments" >
@@ -10,6 +10,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <style>
+        .navbar{
+              background-color:#6495ED;
+        }
+       </style>
 </head> 
 
 <body>
