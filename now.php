c2da0a8f9065b833af2976c34c6504125290e107
romycharbel <45421424+romycharbel@users.noreply.github.com>
2019-05-17 12:14:06 +0300

    Update now.php

--- now.php
+++ now.php
@@ -155,7 +155,7 @@ print($row['datee']);*/
 
       <br>
       <div id="photo" align="center">
-      <img src="undersea.jpg" class="img-fluid" alt="Responsive image" width="400px" height="300px">
+      <img src="undersea.jpg" class="img-fluid" alt="Responsive image" width="800px" height="600px">
       </div>
 </body>  
 </html>
